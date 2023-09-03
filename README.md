74c2cd46fb0d434a46b509a92138be9dd9dbf983
André Luiz Brito M. de Assumpção <69325534+andrebritoassumpcao@users.noreply.github.com>
2023-09-03 21:33:21 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto criado junto ao curso.dev para praticar o desenvolvimento de um trabalho real.
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
 function Home() {
-    return <h1>Se você consegue ler isso digite "Pão com goiabada" no Whats pra mim. </h1>
+	return (
+		<h1>
+			{" "}
+			Se você consegue ler isso digite "Pão com goiabada" no Whats pra mim.
+		</h1>
+	);
 }
 export default Home;
